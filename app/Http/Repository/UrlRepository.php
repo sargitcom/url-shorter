70c4d56382a5b8ea3a1d524256f14ea7b86041f2
Kamil <sargitcom@gmail.com>
2019-10-02 20:24:56 +0200

    add repository of url

---
+++ app/Http/Repository/UrlRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Repository;
+
+class UrlRepository
+{
+    public function createLink(string $link)
+    {
+        $sql = <<<QUERY
+select 
+
+from 
+    
+QUERY;
+
+    }
+
+    public function getLinkById(int $linkId)
+    {
+
+    }
+}
